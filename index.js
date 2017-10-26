4d3c797ba6c495f6ed6867561713886194d49608
eb-za <ebh@EBHs-MacBook-Air.local>
2017-10-26 23:30:27 +0200

    Changed configuration values

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'Dkeu022be1ZClIv7okv5',
+  masterKey: process.env.MASTER_KEY || 'Cw4MPjeBoLwnKaqStCxd', //Add your master key here. Keep it secret!
+  clientKey: 'M0RwXiBPo9YwVcxwHvcr',
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
